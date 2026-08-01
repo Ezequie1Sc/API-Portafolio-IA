24fe3b26321ae71fcb412ff3dbb7aa61de6d06f5
ezequieldevsc <ezequieldevsc@gmail.com>
2026-07-31 18:36:09 -0600

    fix: correcion de config py

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-
-    APP_NAME: str
-    API_VERSION: str
+    APP_NAME: str = "Portfolio IA API"
+    API_VERSION: str = "1.0.0"
+    FRONTEND_URL: str = "http://localhost:5173"
 
     GEMINI_API_KEY: str
 
-    FRONTEND_URL: str
-
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
 
 settings = Settings()
