6571084232c30a7358558c379371ff4e0f0810af
ezequieldevsc <ezequieldevsc@gmail.com>
2026-07-31 19:08:50 -0600

    feat: agregar mejora

--- app/main.py
+++ app/main.py
@@ -22,19 +22,20 @@ async def root():
         "status": "running"
     }
 
+from fastapi import HTTPException
+from google import genai
 
 @app.get("/test-gemini")
 async def test_gemini():
+    try:
+        client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
-    client = genai.Client(
-        api_key=settings.GEMINI_API_KEY
-    )
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents="Responde únicamente: OK"
+        )
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Responde únicamente: OK"
-    )
+        return {"respuesta": response.text}
 
-    return {
-        "respuesta": response.text
-    }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
