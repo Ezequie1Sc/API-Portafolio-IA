cff32b09886fca5d43458873bacaf23050a3e9ce
Ezequie1Sc <ezequielsc017@gmail.com>
2026-08-05 19:26:22 -0600

    feat: agregar logica de profile service

---
+++ app/services/portfolio/profile_service.py
@@ -0,0 +1,12 @@
+from app.services.portfolio.base_data_service import BaseDataService
+
+
+class ProfileService(BaseDataService):
+
+    def __init__(self):
+
+        super().__init__()
+
+    def get(self):
+
+        return self.load_json("profile.json")
